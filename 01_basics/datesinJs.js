fed215d93f9a92a26a9858e17d5257b9af294e49
shopifydev1997 <big99webdeveloper@gmail.com>
2024-12-04 10:53:06 +0000

    date time

---
+++ 01_basics/datesinJs.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 4 , 10)
+// let myCreatedDate = new Date(2023, 4 , 10, 5, 3)
+let myCreatedDate = new Date("04-12-2024");
+console.log(myCreatedDate.toLocaleString());
+console.log(newDate.getDate());
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+    
+})
