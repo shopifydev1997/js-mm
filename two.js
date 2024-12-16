e1b75168bbb0dbc65409b1d2c412b7db10d614c5
shopifydev1997 <big99webdeveloper@gmail.com>
2024-12-16 05:57:15 +0000

    loop

---
+++ two.js
@@ -0,0 +1,43 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+//     if (element == 5) {
+//         console.log("5 is best number");
+        
+//     }
+
+    // console.log(element);
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop ${i}`);
+    
+//     for (let j = 0; j < 10; j++) {
+//         console.log(`inner loop ${j} and inner loop ${i}`);
+        
+//     }
+    
+// }
+
+let index = 0
+
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ["Rahul", "Rohan", "Real"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray [arr]}`);
+//     arr = arr + 1
+// }
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
